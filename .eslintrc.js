152fe2b0298deb7ce6719b0997ca46daf9793753
Ranvijay <ranvijaychouhan12@gmail.com>
2025-08-03 03:02:16 +0530

    Created the Stack and Screens and Corrected there types

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,19 +9,32 @@ module.exports = {
       'warn',
       {
         groups: [
-          ['builtin', 'external', 'internal'],
-          ['parent', 'sibling', 'index'],
+          ['builtin'], // Node.js and third-party packages
+          'external',
+          'internal', // Internal imports (e.g., @redux/hooks, @themes/spacing)
+          ['parent', 'sibling'], // Relative imports
         ],
         pathGroups: [
           {
-            pattern: 'react',
+            pattern:
+              '@{redux,themes,assets,components,conf,hooks,navigation,storage,thunk,slice,screens,themes,colors,utils,models,app,network,common,services,context,constants,skeletonLoader}/**', // Match internal imports
+            group: 'internal',
+            position: 'before', // Ensure internal imports are placed before relative imports
+          },
+          {
+            pattern: '@**', // Match imports starting with @ (like @react-navigation/native, @redux/hooks, etc.)
             group: 'external',
-            position: 'before',
+            position: 'after', // Ensure @ imports come last (after other external packages)
+          },
+          {
+            pattern: 'react**', // Match imports starting with @ (like @react-navigation/native, @redux/hooks, etc.)
+            group: 'builtin',
+            position: 'after', // Ensure @ imports come last (after other external packages)
           },
         ],
-        pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'never',
-        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'never', // Ensure a blank line between groups
+        alphabetize: { order: 'asc', caseInsensitive: true }, // Sort alphabetically
       },
     ],
   },
